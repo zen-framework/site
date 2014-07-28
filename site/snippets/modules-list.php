7a3fa0d993f8a05291be5c6764d7c798dc90166a
Travis Maynard <hello@travismaynard.com>
2014-07-28 11:24:27 -0400

    Added snippets for grid, navigation, and menu modules.

--- site/snippets/modules-list.php
+++ site/snippets/modules-list.php
@@ -1,4 +1,4 @@
-<?php foreach($pages->find('modules')->children()->visible()->flip() as $module): ?>
+<?php foreach($pages->find('modules')->children()->visible() as $module): ?>
     <div class="block">
         <div class="block-image">
             <!-- Can Be Used For Photo Or Icon -->
@@ -8,7 +8,10 @@
             
             <h2><?php echo $module->title() ?></h2>
             <p><?php echo excerpt($module->text(), 300) ?></p>
-            <a href="" class="button">Learn More</a>
+            <ul class="chain">
+                <li><a href="#" class="button">Learn More</a></li>
+                <li><a href="#" class="button">View Snippet</a></li>
+            </ul>
 
         </div>
     </div>
